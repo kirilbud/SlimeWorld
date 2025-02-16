f67e922efc1606ac34c78f18bbc547768e4182d3
kirilbud <kiril.saltz@gmail.com>
2025-02-16 00:24:38 -0800

    init

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Code Practice: Slime World
+// Name: Kiril Saltz
+// Date: 2/16/2025
+
+let config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
+    width: 320,
+    height: 240,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    zoom: 2,
+    scene: [ Overworld ]
+}
+
+const game = new Phaser.Game(config)
